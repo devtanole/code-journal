557fb458ba27778a85ee9a1d5ad2791f80361280
Devin Olesen <devintanielu@gmail.com>
2024-09-16 06:21:33 +0000

    data ts, submit event listener

--- ts/data.ts
+++ ts/data.ts
@@ -1,6 +1,38 @@
-const data = {
-  view: 'entry-form',
-  entries: [],
-  editing: null,
-  nextEntryId: 1,
-};
+interface JournalEntry {
+  title: string;
+  photoUrl: string;
+  notes: string;
+  entryId: number;
+}
+
+interface Data {
+  view: 'entries' | 'entry-form';
+  entries: JournalEntry[];
+  editing: null | JournalEntry;
+  nextEntryId: number;
+}
+
+const dataKey = 'code-journal-data';
+
+const data = readData();
+
+function readData(): Data {
+  let data: Data;
+  const localData = localStorage.getItem(dataKey);
+  if (localData) {
+    data = JSON.parse(localData) as Data;
+  } else {
+    data = {
+      view: 'entry-form',
+      entries: [],
+      editing: null,
+      nextEntryId: 1,
+    };
+  }
+  return data;
+}
+
+function writeData(): void {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem(dataKey, dataJSON);
+}
--- ts/main.ts
+++ ts/main.ts
@@ -10,11 +10,25 @@ if ($formElement == null || $entryImage == null)
   throw new Error('image has failed');
 const formControls = $formElement.elements as FormElements;
 const $photoUrlElement = formControls.photoUrl;
-// const placeholderImage = $entryImage.getAttribute('src') || ''; //
+const placeholderImage = $entryImage.getAttribute('src') || '';
 
 $photoUrlElement.addEventListener('input', (event: Event) => {
   const photoUrl = (event.target as HTMLInputElement).value;
   if (photoUrl != null) {
     $entryImage.setAttribute('src', photoUrl);
   }
+});
+
+$formElement.addEventListener('submit', (event: Event) => {
+  event.preventDefault();
+  const newEntry: JournalEntry = {
+    entryId: data.nextEntryId++,
+    title: formControls.title.value,
+    photoUrl: formControls.photoUrl.value,
+    notes: formControls.notes.value,
+  };
+  data.entries.unshift(newEntry);
+  writeData();
+  $entryImage.setAttribute('src', placeholderImage);
+  $formElement.reset();
 });
