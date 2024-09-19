2facdc021118e063b9a2d6f34a00a3bb82dde18c
Devin Olesen <devintanielu@gmail.com>
2024-09-19 08:01:11 +0000

    renderENtry function complete

--- js/main.js
+++ js/main.js
@@ -1,35 +1,27 @@
-interface FormElements extends HTMLFormControlsCollection {
-  photoUrl: HTMLInputElement;
-  title: HTMLInputElement;
-  notes: HTMLTextAreaElement;
-}
-
-const $formElement = document.querySelector('#entry-form') as HTMLFormElement;
-const $entryImage = document.querySelector('#entry-image') as HTMLImageElement;
+'use strict';
+const $formElement = document.querySelector('#entry-form');
+const $entryImage = document.querySelector('#entry-image');
 if ($formElement == null || $entryImage == null)
   throw new Error('image has failed');
-const formControls = $formElement.elements as FormElements;
+const formControls = $formElement.elements;
 const $photoUrlElement = formControls.photoUrl;
 const placeholderImage = $entryImage.getAttribute('src') || '';
-// const $noEntriesText = document.querySelector('#none');
+const $noEntriesText = document.querySelector('#none');
 const $entryListElement = document.querySelector('.entry-list');
-// const $entryFormViewElement = document.querySelector(
-// 'div[data-view="entry-form"]',
-// );
-
+const $entryFormViewElement = document.querySelector(
+  'div[data-view="entry-form"]',
+);
 if ($formElement == null || $entryImage == null || $entryListElement == null)
   throw new Error('failed');
-
-$photoUrlElement.addEventListener('input', (event: Event) => {
-  const photoUrl = (event.target as HTMLInputElement).value;
+$photoUrlElement.addEventListener('input', (event) => {
+  const photoUrl = event.target.value;
   if (photoUrl != null) {
     $entryImage.setAttribute('src', photoUrl);
   }
 });
-
-$formElement.addEventListener('submit', (event: Event) => {
+$formElement.addEventListener('submit', (event) => {
   event.preventDefault();
-  const newEntry: JournalEntry = {
+  const newEntry = {
     entryId: data.nextEntryId++,
     title: formControls.title.value,
     photoUrl: formControls.photoUrl.value,
@@ -40,47 +32,35 @@ $formElement.addEventListener('submit', (event: Event) => {
   $entryImage.setAttribute('src', placeholderImage);
   $formElement.reset();
 });
-
 // issue 2
-
 document.addEventListener('DOMContentLoaded', () => {
   for (const entry of data.entries) {
     $entryListElement.appendChild(renderEntry(entry));
   }
   // toggleNoEntries();
-  // viewSwap(data.view);
+  //viewSwap(data.view);
 });
-
-function renderEntry(entry: JournalEntry): HTMLLIElement {
+function renderEntry(entry) {
   const $entry = document.createElement('li');
   $entry.setAttribute('data-entry-id', entry.entryId.toString());
-
   const $entryRow = document.createElement('div');
   $entryRow.className = 'row';
-
   const $leftColumn = document.createElement('div');
   $leftColumn.className = 'column-half';
-
   const $listImageDiv = document.createElement('div');
   $listImageDiv.className = 'image';
-
   const $entryImage = document.createElement('img');
   $entryImage.setAttribute('src', entry.photoUrl);
-
   const $rightColumn = document.createElement('div');
   $rightColumn.className = 'column-half';
-
   const $entryTitle = document.createElement('h2');
   $entryTitle.textContent = entry.title;
-
   const $entryNotes = document.createElement('p');
   $entryNotes.textContent = entry.notes;
-
   $entry.append($entryRow);
   $entryRow.append($leftColumn, $rightColumn);
   $leftColumn.append($listImageDiv);
   $listImageDiv.append($entryImage);
   $rightColumn.append($entryTitle, $entryNotes);
-
   return $entry;
 }
